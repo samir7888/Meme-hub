73f19890d6039ab9524a93bb73dd9a04a70d880c
Sameer Basnet <basnetsameer78@gmail.com>
2025-07-08 13:28:12 +0545

    feat: wrap SearchInput component in Suspense for improved loading handling

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { apiClient } from "./components/api-client";
 import { IImage } from "@/models/Image";
 import { Image } from "@imagekit/react";
 import SearchInput from "./components/SearchInput";
-
+import { Suspense } from "react";
 interface IVideo {
   _id: string;
   title: string;
@@ -45,7 +45,9 @@ export default function Home() {
       <h1 className="text-5xl pt-16 font-extrabold mb-5 text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
         <span>Your Meme Templates</span>
       </h1>
-      <SearchInput />
+      <Suspense>
+        <SearchInput />
+      </Suspense>
       <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8 w-full max-w-6xl">
         {images.map((item) => (
           <div
