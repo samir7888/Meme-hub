5165bf1b3ee7ec20c0ce109a453202512163597f
Sameer Basnet <basnetsameer78@gmail.com>
2025-08-25 21:30:01 +0545

    fixed

--- app/page.tsx
+++ app/page.tsx
@@ -9,12 +9,12 @@ export default async function Home({
 }) {
   const resolvedSearchParams = await searchParams;
   return (
-    <main className="space-y-6 flex min-h-screen flex-col items-center p-8 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300">
-      <div className="m-16">
+    <main className="space-y-6 flex min-h-screen flex-col items-center p-1 md:p-8 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white transition-colors duration-300">
+      <div className="my-16">
         <h1 className="text-3xl tracking-tight leading-tight text-center w-full  md:text-7xl pt-16 font-extrabold mb-5 text-transparent bg-clip-text bg-gradient-to-r  from-purple-400 to-pink-600">
           Create. Laugh. Repeat.
         </h1>
-        <p className="text-xl md:text-3xl max-w-3xl mx-auto text-balance text-center mt-10 bg-gradient-to-b from-purple-300 to-pink-500 text-transparent bg-clip-text selection:bg-white selection:text-neutral-500">
+        <p className="text-xl w-full md:text-3xl max-w-3xl mx-auto text-balance text-center mt-10 bg-gradient-to-b from-purple-300 to-pink-500 text-transparent bg-clip-text selection:bg-white selection:text-neutral-500">
           Choose from viral templates or upload your own. Craft memes that break
           the internet – in seconds!
         </p>
